15ebb3f1a64e0d63bc89d9c444add9fa87d70901
Jack Kryger Sørensen <74965501+jeroleski@users.noreply.github.com>
2022-05-26 10:39:25 +0200

    Bootstrap (#5)
    * Added bootstrap to the login, and profile page. The login page now takes parameters changing the login/sign up text
    
    * Nav bar (#4)
    
    * Add navigation bar
    
    * Add a navigation bar
    
    Co-authored-by: Emilia-Victoria <emiliav.helsted@gmail.com>
    
    * made documents only containing the nav bar for all sub pages
    
    Co-authored-by: Emilia-Victoria <emiliav.helsted@gmail.com>

--- public/profile.js
+++ public/profile.js
@@ -1,2 +1,24 @@
 
 
+function fillInData() {
+
+}
+
+function saveData() {
+    let fullName = document.getElementById("fullName").value
+    let birthday = document.getElementById("birthday").value
+    let creditCardNumber = document.getElementById("creditCardNumber").value
+    let creditCardExpiration = document.getElementById("creditCardExpiration").value
+    let creditCardVerify = document.getElementById("creditCardVerify").value
+    let driversLicense = document.getElementById("driversLicense").value
+
+    sessionStorage.setItem("fullName", fullName)
+    sessionStorage.setItem("birthday", birthday)
+    sessionStorage.setItem("creditCardNumber", creditCardNumber)
+    sessionStorage.setItem("creditCardExpiration", creditCardExpiration)
+    sessionStorage.setItem("creditCardVerify", creditCardVerify)
+    sessionStorage.setItem("driversLicense", driversLicense)
+}
+
+
+
