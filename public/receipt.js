276923d5b93b4942698504b8da638a5ca1025a50
jeroleski <74965501+jeroleski@users.noreply.github.com>
2022-05-30 11:46:48 +0200

    The reciept page can show the user the total price for the rent, and every factor playing into it. If no data is found, then the user is directed to current rental

---
+++ public/receipt.js
@@ -0,0 +1,27 @@
+const initialPrice = 50
+const pricePrMinute = 3
+
+function loadReceipt() {
+    let params = new URLSearchParams(window.location.search)
+    let carName = params.get("carName")
+    let timeUsed = params.get("timeUsed")
+
+    if (! timeUsed) {
+        console.log("opening modal")
+        document.getElementById("continueButton").onclick = function () {
+            window.location.href = 'rental.html'
+        }
+        $('#noRentingModal').modal('show')
+        return
+    }
+
+    let usagePrice = timeUsed * pricePrMinute
+    let totalPrice = usagePrice + initialPrice
+
+    document.getElementById("carNameLabel").innerText = carName
+    document.getElementById("dateLabel").innerText = new Date().toLocaleDateString()
+    document.getElementById("usedTimeLabel").innerText = timeUsed
+    document.getElementById("initialPriceLabel").innerText = `${initialPrice}`
+    document.getElementById("usagePriceLabel").innerText = `${usagePrice}`
+    document.getElementById("totalPriceLabel").innerText = `${totalPrice}`
+}
